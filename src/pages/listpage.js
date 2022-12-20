d013fbd099fdd229db04f81626caaada6757ff6c
Anass Moussadi <a.moussadi03@gmail.com>
2022-12-20 14:01:40 +0100

    list page getting ready for api

--- src/pages/listpage.js
+++ src/pages/listpage.js
@@ -3,7 +3,20 @@ import "../css/style.css";
 export default function ListPage() {
   return (
     <>
-      <h1>Lijst</h1>
+      <div className="about-block1">
+        <div>
+          <span>To-do list</span>
+          <h1>Try it out!</h1>
+          <p>
+            A to do list application is a great way to help you stay organized
+            and on top of tasks. It allows you to easily create lists of tasks,
+            add deadlines and reminders, and keep track of progress. You can
+            even create recurring tasks, so you won't forget to do something you
+            need to do regularly. With a to do list application, you'll be able
+            to manage your tasks in an efficient and effective way. Try it out!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
