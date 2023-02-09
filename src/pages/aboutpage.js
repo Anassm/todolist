58266651532ee955d6ba33f759e2d7ebbfc5117d
Anass Moussadi <a.moussadi03@gmail.com>
2023-02-09 19:26:41 +0100

    commit

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -1,13 +1,50 @@
 import "../css/style.css";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function AboutPage() {
   return (
-    <>
-      <center>
-        <h1>This page is in Construction 🛠️🚧</h1>
-        <br></br>
-        <h4 style={{ fontStyle: "italic" }}>niet</h4>
-      </center>
-    </>
+    <div className="about-wrapper">
+      <div className="about-block2">
+        <div className="about-grid-block2">
+          <div className="about-accomplishments-block2">
+            <h2>Our accomplishments</h2>
+            <p>
+              TO DO has become a leader in its field, earning many awards for
+              its innovative products and excellent customer service. Check out
+              more of our projects:
+            </p>
+            <ul>
+              <li>
+                A portfolio website - <a href="https://anassm.com">Anassm</a>
+              </li>
+              <li>
+                An astronomy website - <a href="#">Stargaze</a>
+              </li>
+              <li>
+                Website for Indian restaurant -{" "}
+                <a href="https://restaurant-handi.nl/">Handi</a>
+              </li>
+            </ul>
+            <a
+              className="about-btn-block2"
+              href="https://anassm.com/my-projects"
+            >
+              See more projects →
+            </a>
+          </div>
+          <div>
+            <Player
+              src="https://assets3.lottiefiles.com/packages/lf20_akyUFY9GQo.json"
+              className="player2"
+              background="transparent"
+              speed="0.5"
+              loop
+              controls
+              autoplay
+            ></Player>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
